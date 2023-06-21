b6529eeba2e0a1bc3b1daf5047dce1baee3d3880
Nguyen Quoc Khanh <21520982@ms.uit.edu.vn>
2023-06-21 22:03:30 +0700

    added onClick for add button (1/2)

--- se114-cordova-app/www/js/index.js
+++ se114-cordova-app/www/js/index.js
@@ -1,3 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // var templateNote = document.querySelector("#note.html");
+  // var templateNote_clone = templateNote.content.cloneNode(true);
+  // var templateList = templateNote_clone.querySelector("#notelist.html");
+  // var templateList_clone = templateList.content.cloneNode(true);
+  // var templateAdd = templateNote_clone.querySelector("#addpage.html");
+  // var templateAdd_clone = templateAdd.content.cloneNode(true);
+
+  templateNote = document.getElementById("note.html");
+  templateList = templateNote.content.getElementById("notelist.html");
+  templateAdd = templateNote.content.getElementById("addpage.html");
+
+  // Get the todo list element
+  var todoList = templateList.content.getElementById("todo-list");
+  // Get the new todo input element
+  var newTodoInput = templateAdd.content.getElementById("new-todo");
+  // Get the add button element
+  var addButton = templateAdd.content.getElementById("add-button");
+
+  // Add event listener for the add button
+  addButton.addEventListener("click", function () {
+    console.log("aaaaaaaaa");
+    // var newTodoText = newTodoInput.value.trim();
+    // if (newTodoText !== "") {
+    //   var listItem = document.createElement("ons-list-item");
+    //   listItem.innerHTML = newTodoText;
+
+    //   var deleteButton = document.createElement("ons-button");
+    //   deleteButton.innerHTML = "Delete";
+    //   deleteButton.addEventListener("click", function () {
+    //     listItem.remove();
+    //   });
+
+    //   listItem.appendChild(deleteButton);
+
+    //   todoList.appendChild(listItem);
+    //   newTodoInput.value = "";
+    // }
+  });
+});
+
 window.fn = {};
 
 // open the side drawer/menu
