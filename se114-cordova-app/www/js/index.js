f71cea6220c18b7bea422354c5a3409304356712
Khanh <21520982@ms.uit.edu.vn>
2023-06-23 21:24:32 +0700

    fixed note width

--- se114-cordova-app/www/js/index.js
+++ se114-cordova-app/www/js/index.js
@@ -1,8 +1,4 @@
-
-
 window.fn = {};
-
-
 
 // open the side drawer/menu
 window.fn.open = function () {
@@ -17,8 +13,6 @@ window.fn.load = function (page) {
   content.load(page).then(menu.close.bind(menu));
 };
 
-
-
 function showSelectedDate() {
   const datePicker = document.getElementById("date-picker");
   const selectedDate = datePicker.value;
@@ -32,6 +26,7 @@ function debugOut() {
 
 // navigator.push new page
 function pushNewPage() {
+  console.log("-----");
   var navigator = document.querySelector("ons-navigator");
   navigator.pushPage("template-add");
 }
@@ -42,61 +37,85 @@ function popPage() {
   navigator.popPage();
 }
 
-function displayHomeView()
-{
-  
+function displayHomeView() {
   const daysTag = document.querySelector(".days"),
-  currentDate = document.querySelector(".current-date"),
-  prevNextIcon = document.querySelectorAll(".icons ons-icon");
-  
+    currentDate = document.querySelector(".current-date"),
+    prevNextIcon = document.querySelectorAll(".icons ons-icon");
+
   // getting new date, current year and month
   let date = new Date(),
-  currYear = date.getFullYear(),
-  currMonth = date.getMonth();
-  
+    currYear = date.getFullYear(),
+    currMonth = date.getMonth();
+
   // storing full name of all months in array
-  const months = ["January", "February", "March", "April", "May", "June", "July",
-                "August", "September", "October", "November", "December"];
-  
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
   const renderCalendar = () => {
     let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
-    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
-    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
-    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+      lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+      lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+      lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
     let liTag = "";
-  
-    for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
-        liTag += `<ons-list-item class="inactive">${lastDateofLastMonth - i + 1}</ons-list-item>`;
+
+    for (let i = firstDayofMonth; i > 0; i--) {
+      // creating li of previous month last days
+      liTag += `<ons-list-item class="inactive">${
+        lastDateofLastMonth - i + 1
+      }</ons-list-item>`;
     }
-  
-    for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
-        // adding active class to li if the current day, month, and year matched
-        let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
-                     && currYear === new Date().getFullYear() ? "active" : "";
-        liTag += `<ons-list-item class="${isToday}">${i}</ons-list-item>`;
+
+    for (let i = 1; i <= lastDateofMonth; i++) {
+      // creating li of all days of current month
+      // adding active class to li if the current day, month, and year matched
+      let isToday =
+        i === date.getDate() &&
+        currMonth === new Date().getMonth() &&
+        currYear === new Date().getFullYear()
+          ? "active"
+          : "";
+      liTag += `<ons-list-item class="${isToday}">${i}</ons-list-item>`;
     }
-  
-    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
-        liTag += `<ons-list-item class="inactive">${i - lastDayofMonth + 1}</ons-list-item>`
+
+    for (let i = lastDayofMonth; i < 6; i++) {
+      // creating li of next month first days
+      liTag += `<ons-list-item class="inactive">${
+        i - lastDayofMonth + 1
+      }</ons-list-item>`;
     }
     currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
     daysTag.innerHTML = liTag;
-  }
+  };
   renderCalendar();
-  prevNextIcon.forEach(icon => { // getting prev and next icons
-    icon.addEventListener("click", () => { // adding click event on both icons
-        // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
-        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
-  
-        if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
-            // creating a new date of current year & month and pass it as date value
-            date = new Date(currYear, currMonth, new Date().getDate());
-            currYear = date.getFullYear(); // updating current year with new date year
-            currMonth = date.getMonth(); // updating current month with new date month
-        } else {
-            date = new Date(); // pass the current date as date value
-        }
-        renderCalendar(); // calling renderCalendar function
+  prevNextIcon.forEach((icon) => {
+    // getting prev and next icons
+    icon.addEventListener("click", () => {
+      // adding click event on both icons
+      // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+      currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+
+      if (currMonth < 0 || currMonth > 11) {
+        // if current month is less than 0 or greater than 11
+        // creating a new date of current year & month and pass it as date value
+        date = new Date(currYear, currMonth, new Date().getDate());
+        currYear = date.getFullYear(); // updating current year with new date year
+        currMonth = date.getMonth(); // updating current month with new date month
+      } else {
+        date = new Date(); // pass the current date as date value
+      }
+      renderCalendar(); // calling renderCalendar function
     });
   });
 }
@@ -110,7 +129,6 @@ document.addEventListener("init", function (event) {
   if (page.matches("#home-page")) {
     console.log("Init home page");
     displayHomeView();
-    
   } else if (page.matches("#note-page")) {
     console.log("Init note page");
 
@@ -165,9 +183,7 @@ document.addEventListener("init", function (event) {
     //   });
   } else if (page.matches("#settings-page")) {
     console.log("Init settings page");
-  }
-  else if(page.matches("home-page"))
-  {
+  } else if (page.matches("home-page")) {
     renderCalendar();
   }
 });
@@ -222,5 +238,3 @@ document.addEventListener("init", function (event) {
 //       button.innerText = "abc";
 //     }, 500);
 //   });
-
-
